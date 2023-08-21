3b18493452bc2db8e46842c6505d1c5205332a6e
protick.kumer <protick.kumer@bjitgroup.com>
2023-08-21 17:10:19 +0600

    Added cloud config server and new CurrencyExchange service

---
+++ spring-cloud-configuration-server/src/test/java/com/protick/springcloudconfigurationserver/SpringCloudConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protick.springcloudconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
